0913e228add8a3a65cbae9ec3031b432f9bdada3
Oscardk <dauren2050@yandex.ru>
2024-05-09 17:28:15 +0500

    secret in env

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,8 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=ro&4)-&(g*z-2i02n_4mffj3hyu4hs$w7!g-2(es3nvd-9+m4'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
